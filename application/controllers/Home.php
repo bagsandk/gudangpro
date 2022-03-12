99a242d10eb66acc13f282f6074f1e0827366252
bagsandk <bagas.sageng@gmail.com>
2022-03-12 21:21:24 +0700

    home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -16,7 +16,7 @@ class Home extends CI_Controller
 
   public function index()
   {
-    $data['content'] = 'contact_view';
+    $data['content'] = 'home/index';
     $this->load->view('templates/main', $data);
   }
 }
